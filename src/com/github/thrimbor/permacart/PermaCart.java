f1744d3c74d73bcf532b2c57b21758a6ed3cded1
thrimbor <thrimbor.github@gmail.com>
2013-05-28 23:18:15 +0200

    + initial commit

---
+++ src/com/github/thrimbor/permacart/PermaCart.java
@@ -0,0 +1,73 @@
+package com.github.thrimbor.permacart;
+
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Minecart;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PermaCart extends JavaPlugin implements Listener {
+	private int radius;
+	private boolean keepStationaryCartsLoaded;
+	
+	public void onEnable () {
+		this.radius = this.getConfig().getInt("radius");
+		this.keepStationaryCartsLoaded = this.getConfig().getBoolean("keep_stationary_carts_loaded");
+		
+		getServer().getPluginManager().registerEvents(this, this);		
+		
+		getLogger().info(this.getDescription().getName() + "is now enabled");
+	}
+	
+	public void onDisable () {
+		getLogger().info(this.getDescription().getName() + "is now disabled");
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onMove (VehicleMoveEvent event) {
+		if (event.getVehicle() instanceof Minecart) {
+			Chunk chunk = event.getVehicle().getLocation().getBlock().getChunk();
+			World world = chunk.getWorld();
+			int x = chunk.getX();
+			int z = chunk.getZ();
+			
+			for (int xr = x-radius; xr <= x+radius; xr++)
+				for (int zr = z-radius; zr <= z+radius; zr++)
+					if (!world.isChunkLoaded(xr, zr))
+						world.loadChunk(xr, zr);
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onUnload (ChunkUnloadEvent event) {
+		for (Entity entity : event.getChunk().getEntities()) {
+			if (entity instanceof Minecart) {
+				if (((Minecart)entity).getVelocity().length() > 0) {
+					event.setCancelled(true);
+					return;
+				}
+			}
+		}
+		
+		// if stationary carts should be kept loaded also
+		if (this.keepStationaryCartsLoaded) {
+			int x = event.getChunk().getX();
+			int z = event.getChunk().getZ();
+			World world = event.getWorld();
+			
+			for (int xr = x-radius; xr <= x+radius; xr++)
+				for (int zr = z-radius; zr <= z+radius; zr++)
+					for (Entity entity : world.getChunkAt(xr, zr).getEntities())
+						if (entity instanceof Minecart) {
+							event.setCancelled(true);
+							return;
+						}
+		}
+	}
+
+}
